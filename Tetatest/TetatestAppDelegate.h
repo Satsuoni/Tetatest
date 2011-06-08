f82da830ac969d4a84a066ffc47b3eeb98119338
Seva <seva@sevas-mac.yamat1.kn.home.ne.jp>
2011-06-08 22:07:30 +0900

    Initial Commit

---
+++ Tetatest/TetatestAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TetatestAppDelegate.h
+//  Tetatest
+//
+//  Created by Seva on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TetatestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
