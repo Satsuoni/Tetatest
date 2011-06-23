ad6a3857aba1cbd3ad9472b7c60f671eb86aea98
Seva <seva@satsuni.(none)>
2011-06-23 13:01:41 +0900

    Recommit

---
+++ Tetatest/SVTetrisBody.m
@@ -0,0 +1,14 @@
+//
+//  SVTetrisBody.m
+//  Tetatest
+//
+//  Created by Seva on 6/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SVTetrisBody.h"
+
+
+@implementation SVTetrisBody
+
+@end
